c3a696115ae99497f905bd4d8bf2cdd4b71f6404
aniram112 <maroschupkina_1@edu.hse.ru>
2021-09-14 22:18:40 +0300

    Initial Commit

---
+++ maroschupkina_1PW1/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  maroschupkina_1PW1
+//
+//  Created by Marina Roshchupkina on 14.09.2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
