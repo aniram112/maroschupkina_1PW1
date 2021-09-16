1c4f21806ea1cea3f05a65d9680eb63590ba35b5
aniram112 <maroschupkina_1@edu.hse.ru>
2021-09-16 14:08:33 +0300

    hw1

--- maroschupkina_1PW1/ViewController.swift
+++ maroschupkina_1PW1/ViewController.swift
@@ -8,12 +8,46 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    @IBOutlet var views: [UIView]!
     override func viewDidLoad() {
         super.viewDidLoad()
+        for view in views {
+            view.layer.cornerRadius = 10
+        }
+        
         // Do any additional setup after loading the view.
     }
-
-
+    @IBAction func changeColorButtonPressed(_ sender: Any) {
+        var set = Set<UIColor>()
+        while set.count < views.count {
+            set.insert( UIColor(
+                            red: .random(in: 0...1),
+                            green: .random(in: 0...1),
+                            blue: .random(in: 0...1),
+                            alpha: 1))
+        }
+        
+        let button = sender as? UIButton
+        button?.isEnabled = false
+        
+        
+        UIView.animate(withDuration: 2, animations: {
+            for view in self.views {
+                /*
+                if (view.layer.cornerRadius == 30){ view.layer.cornerRadius = 10}
+                else  {view.layer.cornerRadius = 30}*/
+                view.layer.cornerRadius = CGFloat(Int.random(in:10..<31));
+                view.backgroundColor = set.popFirst()
+            }
+        }) { completion in button?.isEnabled = true}
+        
+        
+        
+        
+        
+    }
+    
+    
 }
 
